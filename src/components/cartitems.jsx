f107cf3de1419e57ae68f8f81edf641d7fdc9bc6
Mehdi Hassar <mido.hass.31@gmail.com>
2023-07-14 00:46:50 +0100

    create cart using usecontext

--- src/components/cartitems.jsx
+++ src/components/cartitems.jsx
@@ -6,58 +6,44 @@ const cartitems = (props) => {
     const {id, name, image, price, brand} = props.data;
     const { viewProductDetails, addToCart, cartItems, removeToCart, updateCartItemCount } = useContext(ShopContext); 
     return <>
-        <section className="cart-item p-5">
-            <div className="container-xxl">
-                <div key={id} className="row">
-                    <table class="table">
-                        <thead>
-                          <tr>
-                            <th>Product Image</th>
-                            <th>Product Details</th>
-                            <th>Edit</th>
-                            <th>Coupons</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>
-                                <img src={image} alt="" className='img-fluid'/>
-                            </td>
-                            <td>
-                                <p>{brand}</p>
-                                <p>{name}</p>
-                                <p>{price}</p>
-                            </td>
-                            <td>
-                                <button
-                                className='add-button'
-                                onClick={() => {
-                                    addToCart(id);
-                                }}
-                                >+</button>
-
-                                <input type="text" value={cartItems[id]} onChange={(e) =>
-                                updateCartItemCount(Number(e.target.value), id)} />
-
-                                <button
-                                className='add-button'
-                                onClick={() => {
-                                    removeToCart(id);
-                                }}
-                                >-</button>
-                            </td>
-                            <td>
-                                <div className="input-group mb-3">
-                                  <input type="text" className="form-control" placeholder="Recipient's username" />
-                                  <span className="input-group-text m-0">Search</span>
-                                </div>
-                            </td>
-                          </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </section>
+        
+                <tbody key={id}>
+                    <tr>
+                      <td className='col-2'>
+                          <div className="p-2">
+                            <img src={image} alt="" className='img-fluid'/>
+                          </div>
+                      </td>
+                      <td className='col-4'>
+                        <p>Produc Brand: <span className="price">{brand}</span></p>
+                        <p>Product Name: <h6 className='price'>{name}</h6></p>
+                        <p>Price: <span className='price'>{price}</span></p>
+                        <p>Items In Stock: <span className="text-danger">1000</span></p>
+                      </td>
+                      <td>
+                          <button
+                          className='add-button mx-2'
+                          onClick={() => {
+                              addToCart(id);
+                          }}
+                          >+</button>
+                          <input type="text" value={cartItems[id]} onChange={(e) =>
+                          updateCartItemCount(Number(e.target.value), id)} />
+                          <button
+                          className='add-button mx-2'
+                          onClick={() => {
+                              removeToCart(id);
+                          }}
+                          >-</button>
+                      </td>
+                      <td>
+                          <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Enter Coupon Code ..." />
+                            <button className="input-group-text m-0">Search</button>
+                          </div>
+                      </td>
+                    </tr>
+                </tbody>
     </>
 }
 
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -7,13 +7,39 @@ import { ShopContext } from '../components/shopcontext';
 const cart = () => {
   const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext); 
   return <>
-    <div className="row">
-        {[...PRODUCTS, ...PRODUCTS1].map((product) => {
-          if(cartItems[product.id] !== 0) {
-            return <Cartitems key={product.id} data={product} />
-          }
-        })}
-    </div>
+    <section className="cart-item p-5">
+      <div className="container-xxl">
+        <div className="row">
+          <table>
+            <thead className='my-2'>
+                <th className='col-3'>Product Image</th>
+                <th className='col-3'>Product Details</th>
+                <th className='col-3'>Edit</th>
+                <th className='col-3'>Coupons</th>
+            </thead>
+
+              {[...PRODUCTS, ...PRODUCTS1].map((product) => {
+                if(cartItems[product.id] !== 0) {
+                  return <Cartitems key={product.id} data={product} />
+                }
+              })}
+
+          </table>
+        </div>
+      </div>
+
+      <div className=" cart-total d-flex justify-content-between">
+        
+        <div className="col-6">
+          Totale : <b>531.2 $</b>
+        </div>
+
+        <div className="col-6">
+          <button>Continue Shopping</button>
+        </div>
+        
+      </div>
+    </section>
   </>
 }
 
